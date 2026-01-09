3956d5e1fdda52b1c9daf9d74dba0c79c3d2a998
Sreypy <sreypydoeung27@gmail.com>
2026-01-09 15:05:29 +0700

    new update TP6

--- e-commerce/routes/web.php
+++ e-commerce/routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +18,31 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::middleware(['auth'])->group(function () {
+
+    Route::post('/products', function () {
+        abort_unless(auth()->user()->can('products.create'), 403);
+        // create product
+    });
+
+    Route::put('/products/{product}', function () {
+        abort_unless(auth()->user()->can('products.update'), 403);
+        // update product
+    });
+
+});
+
+Route::middleware('auth')->group(function () {
+
+    Route::get('/categories/{category}', 
+        [CategoryController::class, 'show']
+    );
+
+    Route::patch('/categories/{category}/status', 
+        [CategoryController::class, 'updateStatus']
+    );
+});
+
 
 require __DIR__.'/auth.php';
